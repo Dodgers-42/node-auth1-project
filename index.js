b9bc73b0450cb1a6a64c25570b765a70dc39628e
Dodger-42 <kevin.bing.barber@gmail.com>
2020-10-28 15:21:30 -0700

    knex. migration, user setup, index, config

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+
+const server = express()
+const port = process.env.PORT || 8080
+
+server.use(express.json())
+
+
+server.listen(port, () => {
+    console.log(`Running at http://localhost:${port}`)
+})
