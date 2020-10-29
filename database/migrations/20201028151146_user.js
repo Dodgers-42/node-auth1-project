1c9320ccd0c2ab0a027e6e502f5fc904665c0250
Dodger-42 <kevin.bing.barber@gmail.com>
2020-10-28 17:30:07 -0700

    working on it

--- database/migrations/20201028151146_user.js
+++ database/migrations/20201028151146_user.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex) {
-  
+const { table } = require("console");
+
+exports.up = async function(knex) {
+  await knex.schema.createTable('users', tbl =>{
+    tbl.increments()
+    tbl.text('username').notNull().unique()
+    tbl.text('password').notNull()
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('users')
 };
